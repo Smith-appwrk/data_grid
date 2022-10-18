8d1e080d91783caf7b2903b2c9449a3cd81618c1
Appwrk <sahilghangash950@gmail.com>
2022-10-18 13:35:55 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,91 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button, TextField } from "@mui/material";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+export default function Home() {
+  const [query, setQuery] = React.useState("");
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const submitQuery = () => {
+    alert(query);
+  };
+  return (
+    <div style={{ width: "80%", margin: "auto", marginTop: "5rem" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "2rem",
+          alignItems: "center",
+          padding: "2rem 1rem",
+          border: "0.2px solid rgba(224, 224, 224, 1)",
+          borderRadius: "3px",
+        }}
+      >
+        <TextField
+          id="outlined-name"
+          label="Enter Query"
+          value={query}
+          onChange={handleChange}
+        />
+        <Button
+          variant="contained"
+          onClick={submitQuery}
+          style={{ backgroundColor: "#121212" }}
+        >
+          Submit
+        </Button>
+      </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
